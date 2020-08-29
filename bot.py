83d0c875340afc92735b529fde283e89e07c7f4e
Aryamaan08 <44227544+Aryamaan08@users.noreply.github.com>
2020-08-29 22:27:55 +0530

    Added custom prefixes
    Also added prefixes.json and changed the help command. Also added -choose command which wasn't there in the -help. Also made it so that -help dms you instead of sending it in the channel.

--- bot.py
+++ bot.py
@@ -1,91 +1,142 @@
 import discord
 from discord.ext import commands
-import random, asyncio
+import random, asyncio, json
 
 token = list(open("token.txt"))[0]
-client = commands.Bot(command_prefix="/")
-client.remove_command('help')
 responses = list(open("8ball.txt"))
 fresponses = list(open("fortune.txt"))
+
+def get_prefix(client, message):
+    with open("prefixes.json") as f:
+        prefixes = json.load(f)
+    return prefixes[str(message.guild.id)]
+
+client = commands.Bot(command_prefix=get_prefix)
+client.remove_command('help')
 
 @client.event
 async def on_ready():
     print("Ready!")
+    
+@client.event
+async def on_guild_join(guild):
+    with open("prefixes.json") as f:
+        prefixes = json.load(f)
+    prefixes[str(guild.id)] = "-"
+    with open("prefixes.json", "w") as f:
+        json.dump(prefixes, f, indent=4)
 
+@client.event
+async def on_guild_remove(guild):
+    with open("prefixes.json") as f:
+        prefixes = json.load(f)
+    prefixes.pop(str(guild.id))
+    with open("prefixes.json") as f:
+        json.dump(prefixes, f, indent=4)
+
+@client.event
+async def on_message(message):
+    if message.content == "<@748739858091999363>":
+        channel = message.channel
+        await channel.send(f"Hi! My prefix for this guild is {get_prefix(client, message)}")
+    await client.process_commands(message)
+
+@commands.guild_only()
+@client.command(aliases=["setprefix", "versaprefix"])
+@commands.has_permissions(administrator=True)
+async def prefix(ctx, *, prefix):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+    prefixes[str(ctx.guild.id)] = prefix
+    with open("prefixes.json", "w") as f:
+        json.dump(prefixes, f)
+    
+    await ctx.send("Done!")
+
+@commands.guild_only()
 @client.command(aliases=["tutorial", "commands", "allcommands"])
 async def help(ctx):
-    e = discord.Embed(title="List of Commands", color=0x00ff00, footer="Wow, that was a lot to take in! I hope you remember this lol")
+    e = discord.Embed(title="List of Commands", color=0x00ff00)
     e.add_field(
-        name= "/help",
+        name= "-help",
         value= "Sends this prompt"
     )
     e.add_field(
-        name= "/ping",
+        name= "-ping",
         value= "Shows bot ping"
     )
     e.add_field(
-        name= "/8ball, /_8ball <question>",
+        name= "-8ball, -_8ball <question>",
         value= "Ask and you shall receive... Funny responses! 🤣"
     )
     e.add_field(
-        name= "/fortunecookie, /fort, /fortune",
+        name= "-fortunecookie, -fort, -fortune",
         value= "Tells you some fortune (please don't take it seriously)"
     )
     e.add_field(
-        name= "/cool, /coolmetre, /howcool",
+        name= "-cool, -coolmetre, -howcool",
         value= "Shows how cool you are. I hope you get more than 80%!"
     )
     e.add_field(
-        name= "/evil, /evilmetre, /howevil",
+        name= "-evil, -evilmetre, -howevil",
         value= "Shows how R00D you are 😈. Please just get below 20% 😇"
     )
     e.add_field(
-        name= "/die, /dice, /roll, /rolldie, /rolladie",
+        name= "-die, -dice, -roll, -rolldie, -rolladie",
         value= "Unlike the number of aliases, this die actually has 6 faces! (Hey that rhymes)"
     )
     e.add_field(
-        name= "/coin, /flip, /coinflip, /flipcoin, /flipacoin",
+        name= "-coin, -flip, -coinflip, -flipcoin, -flipacoin",
         value= "Honestly at this point you know what a coin is"
     )
     e.add_field(
-        name= "/rahman",
-        value= "The one and only!"
+        name="-choose, -randomguy",
+        value= "Choose a random Poké— I mean human"
     )
     e.add_field(
-        name= "/tungsten, /stupid, /tungstenisstupid",
+        name= "-tungsten, -stupid, -tungstenisstupid",
         value= "The one and only... stupid person 😜"
     )
+    e.set_footer(text="Wow, that was a lot to take in! I hope you remember this lol")
 
-    await ctx.send(embed=e)
+    await ctx.author.send(embed=e)
+    await ctx.author.send("Note that - is the default prefix. Ping @Versa#3702 to get the prefix for your guild (although you already know the prefix cuz you used this command)")
+    await ctx.send("Sent you a DM with the info!")
 
+@commands.guild_only()
 @client.command()
 async def ping(ctx):
     await ctx.send(f"{round(client.latency, 2)}ms")
 
+@commands.guild_only()
 @client.command(aliases=["8ball"])
 async def _8ball(ctx, *, question):
     await ctx.send(random.choice(responses))
 
+@commands.guild_only()
 @client.command(aliases=["fortunecookie", "fort"])
 async def fortune(ctx):
     await ctx.send(random.choice(fresponses))
 
+@commands.guild_only()
 @client.command(aliases=["cool", "coolmetre"])
 async def howcool(ctx):
     coolness = random.randint(0, 100)
-    s = f"{ctx.author[:-5]} is {coolness}% cool!"
+    s = f"{str(ctx.author)[:-5]} is {coolness}% cool!"
     if coolness > 79:
         s += " Wow!"
     await ctx.send(s)
 
+@commands.guild_only()
 @client.command(aliases=["evil", "evilmetre"])
 async def howevil(ctx):
     evilness = random.randint(0, 100)
-    s = f"{ctx.author[:-5]} is {evilness}% evil!"
+    s = f"{str(ctx.author)[:-5]} is {evilness}% evil!"
     if evilness > 79:
         s += " R00D!"
     await ctx.send(s)
 
+@commands.guild_only()
 @client.command(aliases=["roll", "rolldie", "rolladie", "dice"])
 async def die(ctx):
     number = random.randint(0, 6)
@@ -94,6 +145,7 @@ async def die(ctx):
         s += " Awesome! Just don't get it thrice in a row. 😫"
     await ctx.send(s)
 
+@commands.guild_only()
 @client.command(aliases=["flip", "flipcoin", "coinflip", "flipacoin"])
 async def coin(ctx):
     face = random.randint(0, 1)
@@ -104,14 +156,12 @@ async def coin(ctx):
     s = f"{face}! I hope you won your bet 😉"
     await ctx.send(s)
 
-@client.command()
-async def rahman(ctx):
-    await ctx.send("Rahman is awesome!")
-
+@commands.guild_only()
 @client.command(aliases=["stupid", "tungstenisstupid"])
 async def tungsten(ctx):
     await ctx.send("Tungsten is a dum dum 🤣\n*cling clang*")
 
+@commands.guild_only()
 @client.command(aliases=["randomguy"])
 async def choose(ctx):
     await ctx.send(f"{random.choice(ctx.guild.members).name}, I choose you!")
